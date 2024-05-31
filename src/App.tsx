3e588b23a99ba8dd9517fbaf7e8034724c27ebf5
Alex Nascimento <alexjose68@hotmail.com>
2024-05-31 11:13:54 -0300

    feat: Application Layout Structure and Style ready

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,21 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <h1>Descubra sua idade</h1>
-    </div>
+    <main>
+      <h1 className='title' >Descubra sua idade</h1>
+      <div className='container' >
+
+        <label className='label' htmlFor="" >Digite seu nome:</label>
+        <input className="input" type="text" placeholder='Digite o seu nome' />
+
+        <label className='label' htmlFor="" >Digite o ano que nasceu:</label>
+        <input className="input" type="text" placeholder='Digite o ano do nascimento...' />
+
+        <button className='button' >Descobrir idade</button>
+
+      </div>
+      <h2 className='resultado' >Alex Nascimento você tem: 27 anos</h2>
+    </main>
   )
 }
 
